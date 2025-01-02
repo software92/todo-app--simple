c8063ef828f401bf6f0eded6fcee7bcc9c0ab740
jsw <csw9201@naver.com>
2025-01-02 14:40:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header>To Do List</header>
+      <section>
+        <p>item</p>
+        <p>item</p>
+      </section>
+      <h1 className='underline'>test</h1>
+    </>
+  );
+}
+
+export default App;
